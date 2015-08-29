541613ecba8c39c53e0207fad0c3176e4f4f0e24
Sebastiano Mandalà <ssense@tiscali.it>
2015-08-29 21:17:05 +0100

    improve composition root

--- Scripts/GameContext.cs
+++ Scripts/GameContext.cs
@@ -9,41 +9,40 @@ using Svelto.Ticker;
 
 public class Main:ICompositionRoot
 {
-	public Svelto.IoC.IContainer container { get; private set; }
-	
-	public Main()
-	{
-		HOTween.Init(false, false, true);
-		HOTween.EnableOverwriteManager();
-		
-		SetupContainer();
-		StartGame();
-	}
-	
-	void SetupContainer()
-	{
-		container = new Container();
-		
-		container.Bind<IGameObjectFactory>().AsSingle<GameObjectFactory>(new GameObjectFactory(container));
+    public Svelto.IoC.IContainer container { get; private set; }
+    
+    public Main()
+    {
+        HOTween.Init(false, false, true);
+        HOTween.EnableOverwriteManager();
+        
+        SetupContainer();
+        StartGame();
+    }
+    
+    void SetupContainer()
+    {
+        container = new Container();
+        
+        container.Bind<IGameObjectFactory>().AsSingle(new GameObjectFactory(container));
         container.Bind<IMonsterCounter>().AsSingle<UnderAttackSystem>();
 
         container.Bind<WeaponPresenter>().ToFactory(new MultiProvider<WeaponPresenter>());
         container.Bind<MonsterPresenter>().ToFactory(new MultiProvider<MonsterPresenter>());
 
         container.BindSelf<UnderAttackSystem>();
-		container.BindSelf<PathController>();
-		container.BindSelf<MonsterSpawner>();
-	}
-	
-	void StartGame()
-	{
-		//tickEngine could be added in the container as well
-		//if needed to other classes!
+        container.BindSelf<PathController>();
+    }
+    
+    void StartGame()
+    {
+        //tickEngine could be added in the container as well
+        //if needed to other classes!
         UnityTicker tickEngine = new UnityTicker(); 
-		
-		tickEngine.Add(container.Build<MonsterSpawner>());
+        
+        tickEngine.Add(container.Inject(new MonsterSpawner()));
         tickEngine.Add(container.Build<UnderAttackSystem>());
-	}
+    }
 }
 
 //A GameObject containing GameContext must be present in the scene
